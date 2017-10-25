c395d01efe7e831187955d18563470ec0ecbaf57
Simke9445 <simke9445@gmail.com>
2017-10-26 00:52:42 +0200

    reverted to react15, react16 has unresolved issues with production build
    Signed-off-by: Simke9445 <simke9445@gmail.com>

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-15';
 
 Enzyme.configure({ adapter: new Adapter() });
 
